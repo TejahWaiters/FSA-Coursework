d22d8fe76774d9560fc9a5994ac99d45d513bc5a
tejahwaiters <tejahmwts@gmail.com>
2024-11-19 17:41:21 -0600

    Save work in progress

---
+++ Coursework/block17/script.js
@@ -0,0 +1,9 @@
+/*A function called "multiplication" that returns the product of the two input numbers.
+
+A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+Example: concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1])
+...should result in [-1, 1, 3, 9, 15]
+Think about the following; you may need to make assumptions or decisions about the prompt and how the code should behave:
+What should happen with unexpected inputs?
+What kinds of unexpected inputs should we worry about?
+What should happen when there are multiples of the same odd number in the arrays? (Hint: We gave you the answer to this one in the example above.)
