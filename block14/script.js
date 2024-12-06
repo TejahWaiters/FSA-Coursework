30bc039e51b6769864be43b69636499916638bbc
tejahwaiters <tejahmwts@gmail.com>
2024-12-05 23:36:37 -0600

    Assignment Update

--- block14/script.js
+++ block14/script.js
@@ -1,3 +1,124 @@
-const myInstruments = ["flute", "clarinet", "saxophone","trumpet","trombone"]
+// === DO NOT EDIT THIS REGION ===
+// Read the comments to understand how the program is structured.
 
-console.table(myInstruments)
+// Prompt the user for a list of integers separated by commas.
+const userInputString = prompt(
+    "Please enter some integers separated by commas.",
+    "1,2,3,4,5"
+  );
+  
+  // Split the string of numbers into an array of strings.
+  const stringArray = userInputString.split(",");
+  
+  // Convert the array of strings into an array of numbers.
+  const numbers = [];
+  for (let i = 0; i < stringArray.length; i++) {
+    const str = stringArray[i];
+    const number = parseInt(str);
+    numbers.push(number);
+  }
+  
+  // Peform some calculations on the numbers.
+  console.log(numbers);
+  console.log(`You have given ${getLength(numbers)} numbers.`);
+  console.log(`The sum of your numbers is ${getSum(numbers)}.`);
+  console.log(`The mean of your numbers is ${getMean(numbers)}.`);
+  console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
+  console.log(`The largest of your numbers is ${getMax(numbers)}.`);
+  console.log(`The range of your numbers is ${getRange(numbers)}.`);
+  console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
+  console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
+  
+  // === EDIT THE CODE BELOW ===
+  // Complete the functions below to make the program work!
+  
+  /**
+   * @param {number[]} numbers an array of integers
+   * @returns {number} the length of the array
+   */
+  function getLength(numbers) {
+  return numbers.length;
+  }
+  
+  /**
+   * @param {number[]} numbers an array of integers
+   * @returns {number} the sum of the numbers
+   */
+  function getSum(numbers) {
+   let sum = 0;
+   for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+   }
+   return sum;
+  }
+  
+  /**
+   * @param {number[]} numbers an array of integers
+   * @returns {number} the mean of the numbers
+   */
+  function getMean(numbers) {
+  if (numbers.length === 0) 
+  return 0;
+
+  let sum = 0;
+  for (let i = 0; i< numbers.length; i++){
+    sum += numbers[i];
+  }
+  return sum/numbers.length;
+  }
+  
+  /**
+   * @param {number[]} numbers an array of integers
+   * @returns {number} the smallest of the numbers
+   */
+  function getMin(numbers) {
+    if (numbers.length === 0) 
+    return undefined;
+    return Math.min(...numbers);
+  }
+  
+  /**
+   * @param {number[]} numbers an array of integers
+   * @returns {number} the largest of the numbers
+   */
+  function getMax(numbers) {
+  if (numbers.length === 0)
+  return undefinded;
+  return Math.max(...numbers);
+  }
+  
+  /**
+   * @param {number[]} numbers an array of integers
+   * @returns {number} the range of the numbers (max - min)
+   */
+  function getRange(numbers) {
+    if (numbers.length === 0) 
+    return 0;
+
+    let max = Math.max(...numbers);
+    let min = Math.min(...numbers);
+
+    return max-min;
+  }
+  
+  /**
+   * @param {number[]} numbers an array of integers
+   * @returns {number[]} the even numbers in the array
+   */
+  function getEvens(numbers) {
+    let getEvens = [];
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 === 0) {
+        getEvens.push(numbers[i]);
+      }
+    }
+    return getEvens;
+  }
+  
+  /**
+   * @param {number[]} numbers an array of integers
+   * @returns {number[]} the odd numbers in the array
+   */
+  function getOdds(numbers) {
+  return numbers.filter(numbers => numbers % 2 !== 0);
+  }
