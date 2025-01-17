cf8d7b470d7f010c4777e9079a35a60af4ef6d2c
tejahwaiters <tejahmwts@gmail.com>
2025-01-16 20:08:06 -0600

    Assignment Update

---
+++ block19/index.js
@@ -0,0 +1,37 @@
+const averageprice = document.querySelector("#average-price");
+const table = document.querySelector("#table tbody");
+
+const freelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
+
+function showFreelancers() {
+  freelancers.forEach((elem) => {
+    const row = document.createElement("tr");
+
+    const name = document.createElement("td");
+    name.textContent = elem.name;
+    row.appendChild(name);
+
+    const price = document.createElement("td");
+    price.textContent = `$${elem.price}`;
+    row.appendChild(price);
+
+    const occupation = document.createElement("td");
+    occupation.textContent = elem.occupation;
+    row.appendChild(occupation);
+
+    table.appendChild(row);
+  });
+}
+
+function generateRandomFreelancer = 
+
+showFreelancers();
